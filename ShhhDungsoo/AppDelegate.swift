94cc5a77caefb4f3d77cd132b29656b29424de12
Kyujin Cho <kyujin.cho@samkrion.com>
2020-08-28 17:32:34 +0900

    Initial Commit

---
+++ ShhhDungsoo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ShhhDungsoo
+//
+//  Created by Kyujin Cho on 2020/08/28.
+//  Copyright © 2020 Kyujin Cho. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
